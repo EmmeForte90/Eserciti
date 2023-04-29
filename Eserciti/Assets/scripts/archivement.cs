508a228f0616fea77376d6c8db945ff03518c710
Beerman84 <cappelloeros@gmail.com>
2023-04-29 20:11:49 +0200

    cambiamenti unity android + bug fix

--- Eserciti/Assets/scripts/archivement.cs
+++ Eserciti/Assets/scripts/archivement.cs
@@ -1,45 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Steamworks;
+#if !UNITY_ANDROID
+    using Steamworks;
+#endif
 
 public class archivement : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
+#if !UNITY_ANDROID
         if(SteamManager.Initialized) {
             string name = SteamFriends.GetPersonaName();
             //Debug.Log(name);
         }
-        /*
-        try{
-            Steamworks.SteamClient.Init(2369250);
-            Debug.Log("My Steam State: " + SteamClient.State);
-            Debug.Log("My Steam is valid: " + SteamClient.IsValid );
-            Debug.Log("My Steam is loggedon: " + SteamClient.IsLoggedOn );
-        }
-        catch (System.Exception e){
-            print (e);
-        }
-        */
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        /*
-        if (Input.GetKeyDown(KeyCode.Space)){
-            archivia_premio("reach_stage_30");
-        }
-        */
+#endif
     }
 
     public void archivia_premio(string id_premio){
+#if !UNITY_ANDROID
         if (!SteamManager.Initialized){print ("errore: Archive non pronto");return;}
         string name = SteamFriends.GetPersonaName();
-        Debug.Log("nome: "+name+" - "+id_premio);
+        Debug.Log("nome giocatore: "+name+" - "+id_premio);
         SteamUserStats.SetAchievement(id_premio);
         SteamUserStats.StoreStats();
+#endif
     }
 }
